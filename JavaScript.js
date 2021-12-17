48ec668b31187377d05fcccea0a517efec17969b
Martina Tiso <martina.tiso@avanade.com>
2021-12-17 15:16:33 +0100

    fine

---
+++ JavaScript.js
@@ -0,0 +1,35 @@
+﻿let insered = false;
+
+
+function save() {
+    let email = document.getElementById("emailInput").value;
+    localStorage.setItem('email', email);
+    alert("Registrazione effetuata ");
+
+    
+}
+
+
+function checktext() {
+    let email = localStorage.getItem("email")
+    if (email != null) {
+        insered = true;
+        let button = document.getElementById("unsubscribe");
+        button.disabled = Insered;
+    } else
+        let button = document.getElementById("unsubscribe");
+        button.disabled = Insered;
+
+}
+
+function registrazione() {
+
+    let email = localStorage.getItem("email");
+
+}
+function cancella() {
+    localStorage.clear();
+    document.getElementById("subscribe").style.display = "block";
+    document.getElementById("unsubscribe").style.display = "none";
+
+}
